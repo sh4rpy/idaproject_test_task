693911a0ca035579b44479cb1c2036c705a72915
Никита Левашов <sh4rpy@ya.ru>
2021-02-06 16:36:20 +0300

    fixed a bug with the inability to enlarge the image only by width after decreasing

--- images/utils.py
+++ images/utils.py
@@ -9,12 +9,12 @@ from .models import Image as ImageModel
 
 def resize_image(source: ImageModel, width: int, height: int) -> None:
     """Изменяет размер исходного изображения"""
+    if width is None:
+        width = height
+    if height is None:
+        height = width
     output_io_stream = BytesIO()
     image = Image.open(source.original_image)
-    if width is None:
-        width = source.resized_image.width
-    if height is None:
-        height = source.resized_image.height
     image.thumbnail((width, height), Image.BILINEAR)
     os.remove(source.resized_image.path)
     image.save(output_io_stream, format=image.format, quality=100)
